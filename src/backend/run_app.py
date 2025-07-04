76f865e8c0cc37194189a1f137ab60e76bc15c2e
Alexandre Caby <alexandre.caby@ig2i.centralelille.fr>
2025-07-04 10:57:07 +0200

    fix: files import

--- src/backend/run_app.py
+++ src/backend/run_app.py
@@ -1,9 +1,17 @@
 import os
 import sys
+import importlib
 
-# Add the project root to Python path
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Get directory paths
+file_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(file_dir)))
 sys.path.insert(0, project_root)
+
+sys.modules['src'] = importlib.import_module('secure_src')
+sys.modules['src.utils'] = importlib.import_module('secure_src.utils')
+sys.modules['src.utils.config'] = importlib.import_module('secure_src.utils.config')
+sys.modules['src.core'] = importlib.import_module('secure_src.core')
+sys.modules['src.models'] = importlib.import_module('secure_src.models')
 
 import app
 if __name__ == "__main__":
