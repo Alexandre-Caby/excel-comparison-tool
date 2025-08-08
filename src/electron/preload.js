ee9153ba0adaf6e45f1d85c5b6e8ab28266420b3
Alexandre Caby <alexandre.caby@ig2i.centralelille.fr>
2025-08-08 09:11:29 +0200

    fix axios

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -1,5 +1,4 @@
 const { contextBridge } = require('electron');
-const { marked } = require('marked');
 
 contextBridge.exposeInMainWorld('api', {
     loadUserGuide: async () => {
@@ -27,11 +26,30 @@ contextBridge.exposeInMainWorld('api', {
     },
 
     markdownToHtml: (markdown) => {
+        if (!markdown) return '';
+        
         try {
-            return marked.parse(markdown || '');
+            return markdown
+                .replace(/^# (.+)$/gm, '<h1>$1</h1>')
+                .replace(/^## (.+)$/gm, '<h2>$1</h2>')
+                .replace(/^### (.+)$/gm, '<h3>$1</h3>')
+                .replace(/^#### (.+)$/gm, '<h4>$1</h4>')
+                .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+                .replace(/\*(.+?)\*/g, '<em>$1</em>')
+                .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2" target="_blank" class="markdown-link">$1</a>')
+                .replace(/^- (.+)$/gm, '<li>$1</li>')
+                .replace(/^\d+\. (.+)$/gm, '<li class="numbered">$1</li>')
+                .replace(/\n\n/g, '</p><p>')
+                .replace(/^(?!<[h|l|a])(.+)$/gm, '<p>$1</p>')
+                .replace(/<p><h/g, '<h')
+                .replace(/<\/h([1-6])><\/p>/g, '</h$1>')
+                .replace(/<p><li>/g, '<ul><li>')
+                .replace(/<p><li class="numbered">/g, '<ol><li>')
+                .replace(/<\/li><\/p>/g, '</li></ul>')
+                .replace(/<\/li><\/p>/g, '</li></ol>');
         } catch (error) {
             console.error('Error parsing markdown:', error);
-            return markdown || '';
+            return markdown;
         }
     }
 });
